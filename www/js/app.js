49f1079b77cb7402a41c27e79dd2a68d65e714d3
jacobpaine <painejacob@gmail.com>
2016-03-23 15:28:29 -0500

    All major functionality complete

--- www/js/app.js
+++ www/js/app.js
@@ -22,14 +22,19 @@ angular.module('starter', ['ionic','ngCordova'])
 })
 
 
-
+.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'http://10.**',
+    'http://192.168.**'
+  ]);
+})
 
 
 .controller('HomeCtrl', ['$http', '$scope','$cordovaBarcodeScanner','$ionicPlatform',function($http, $scope, $cordovaBarcodeScanner, $ionicPlatform) {
-
-
   $scope.action = 'http://10.0.0.89:3000/books';
-
   $scope.scan = function(){
     $ionicPlatform.ready(function() {
       $cordovaBarcodeScanner.scan().then(function(result) {
